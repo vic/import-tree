2595e7007a54e1b3994e13fad47b2ecbb6ac88bc
Victor Borja <vborja@apache.org>
2025-03-13 16:45:35 -0600

    filter on path strings

--- default.nix
+++ default.nix
@@ -5,13 +5,19 @@ let
 
   leafs =
     lib: root:
+    let
+      # using toString prevents the path from being copied to the store (and exist)
+      hasSuffix = a: b: lib.hasSuffix a (lib.toString b);
+      hasInfix = a: b: lib.hasInfix a (lib.toString b);
+      notHasInfix = a: b: !hasInfix a b;
+    in
     lib.pipe root [
       (lib.toList)
       (lib.lists.flatten)
       (lib.map lib.filesystem.listFilesRecursive)
       (lib.lists.flatten)
-      (lib.filter (lib.hasSuffix ".nix"))
-      (lib.filter (if filter == null then (i: !lib.hasInfix "/_" i) else filter))
+      (lib.filter (hasSuffix ".nix"))
+      (lib.filter (if filter == null then (notHasInfix "/_") else filter))
     ];
 
   # module exists so we delay access to lib til we are part of the module system.
