44bf74a6369292483850d8c197e1b090aa8bea63
Victor Borja <vborja@apache.org>
2025-03-13 15:32:27 -0600

    nicer to read

--- default.nix
+++ default.nix
@@ -1,21 +1,21 @@
 {
   filter ? null,
 }:
-path: {
-  imports =
-    let
-      inner =
-        { lib, ... }:
-        {
-          imports = lib.pipe path [
-            (lib.toList)
-            (lib.lists.flatten)
-            (lib.map lib.filesystem.listFilesRecursive)
-            (lib.lists.flatten)
-            (lib.filter (lib.hasSuffix ".nix"))
-            (lib.filter (if filter == null then (i: !lib.hasInfix "/_" i) else filter))
-          ];
-        };
-    in
-    [ inner ];
+path:
+let
+  module =
+    { lib, ... }:
+    {
+      imports = lib.pipe path [
+        (lib.toList)
+        (lib.lists.flatten)
+        (lib.map lib.filesystem.listFilesRecursive)
+        (lib.lists.flatten)
+        (lib.filter (lib.hasSuffix ".nix"))
+        (lib.filter (if filter == null then (i: !lib.hasInfix "/_" i) else filter))
+      ];
+    };
+in
+{
+  imports = [ module ];
 }
