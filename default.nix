222d527b8bf96271808a556b8141d1726f9806eb
Victor Borja <vborja@apache.org>
2025-03-13 08:16:55 -0600

    lib

---
+++ default.nix
@@ -0,0 +1,7 @@
+path: {lib, ...}: {
+  imports = lib.pipe path [
+    lib.filesystem.listFilesRecursive
+    (lib.filter (lib.hasSuffix ".nix"))
+    (lib.filter (lib.hasInfix "/_"))
+  ];
+}
---
+++ flake.nix
@@ -0,0 +1,5 @@
+{
+  outputs = _: { 
+    __functor: _: import ./default.nix; 
+  }
+}
