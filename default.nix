3a26361a97c36bb755813dd554ad786e3ed778df
Victor Borja <vborja@apache.org>
2025-03-15 23:38:23 -0600

    Correctly expose flakeModule for nix-unit

--- default.nix
+++ default.nix
@@ -68,7 +68,7 @@ let
         __functor = self: f: {
           config = (f self);
           __functor = functor;
-          flakeModule.nix-unit = ./checks/flakeModule.nix;
+          flakeModules.nix-unit = ./checks/flakeModule.nix;
 
           withLib = lib: self (c: (f c) // { inherit lib; });
 
