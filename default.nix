dea41b155a7f0efb3b21d43c75fec2db17a569bb
Victor Borja <vborja@apache.org>
2025-10-22 02:02:51 -0600

    dont filter on root prefix.
    for example, if user does `import-tree ./_foo`,
    
    explicitly asking to traverse `./_foo`, then results must be
    `./_foo/bar.nix` but not `./_foo/_baz.nix`.
    
    that is, filtering occurs only on after-root paths.

--- default.nix
+++ default.nix
@@ -27,9 +27,10 @@ let
         };
 
       leafs =
-        lib: root:
+        lib:
         let
           treeFiles = t: (t.withLib lib).files;
+
           listFilesRecursive =
             x:
             if isImportTree x then
@@ -40,19 +41,66 @@ let
               lib.filesystem.listFilesRecursive x
             else
               [ x ];
+
           nixFilter = andNot (lib.hasInfix "/_") (lib.hasSuffix ".nix");
+
           initialFilter = if initf != null then initf else nixFilter;
+
           pathFilter = compose (and filterf initialFilter) toString;
+
           otherFilter = and filterf (if initf != null then initf else (_: true));
+
           filter = x: if isPathLike x then pathFilter x else otherFilter x;
+
+          isFileRelative =
+            root:
+            { file, rel }:
+            if file != null && lib.hasPrefix root file then
+              {
+                file = null;
+                rel = lib.removePrefix root file;
+              }
+            else
+              { inherit file rel; };
+          getFileRelative = { file, rel }: if rel == null then file else rel;
+
+          makeRelative =
+            roots:
+            lib.pipe roots [
+              (lib.lists.flatten)
+              (builtins.filter isDirectory)
+              (builtins.map builtins.toString)
+              (builtins.map isFileRelative)
+              (fx: fx ++ [ getFileRelative ])
+              (
+                fx: file:
+                lib.pipe {
+                  file = builtins.toString file;
+                  rel = null;
+                } fx
+              )
+            ];
+
+          rootRelative =
+            roots:
+            let
+              mkRel = makeRelative roots;
+            in
+            x: if isPathLike x then mkRel x else x;
         in
+        root:
         lib.pipe
           [ paths root ]
           [
             (lib.lists.flatten)
             (map listFilesRecursive)
             (lib.lists.flatten)
-            (builtins.filter filter)
+            (builtins.filter (
+              compose filter (rootRelative [
+                paths
+                root
+              ])
+            ))
             (map mapf)
           ];
 
